8a0899778ab18bfc6a3fa498be7e94c7bf3241fd
Hiroki Nagashima <hnagashima.r@gmail.com>
2020-04-22 19:24:20 +0900

    myFFT 説明文を更新

--- myFFT.m
+++ myFFT.m
@@ -1,7 +1,20 @@
 function [freq,FT] = myFFT(x,varargin)
-%f = Fs*(0:(L/2))/L;
-% [freq,FT] = myFFT(x,y,NFFT,dim)
-% [freq,FT] = myFFT(x,y) % non-zero filling
+% myFFT performs Fourier transformation and x-axis conversion.
+% [freq,FT] = myFFT(x,y) % Fourier transformation
+% [____,__] = myFFT(___,NFFT) % zero-padding.
+% [____,__] = myFFT(________,dim) % specify dimension
+% 
+% Input:
+% x: time domain vector.
+% y: data (vector or matrix)
+% NFFT: performs NFFT-point fft. (padding with zeros)
+% DIM: applies the fft operation across the dimension DIM.
+% 
+% Output:
+% f = Fs*(0:(L/2))/L; (Fs; sampling frequency, L number of datas)
+% FT = Fourier transformation data.
+%
+
 
 narginchk(2,4);
 Fs = 1/(x(1) - x(2))/2;
