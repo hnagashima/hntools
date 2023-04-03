bf91dcdf5498261d2a237fd48ff712abcc1ee22b
Hiroki Nagashima <hnagashima.r@gmail.com>
2023-04-03 19:19:23 +0900

    cmenuの依存性解析プログラムをさらに発展, clipboardだけでなく, ファイルとして保存ができるように改善。

--- cmenu.m
+++ cmenu.m
@@ -117,18 +117,21 @@ end
 % add label menu
 label.addLabelMenu(src,event, mh);
 
+% add Figure size fix menu.
+%eh4 = uimenu(mh, LabelName, 'Command for fix Figure Size');
+
 
 %---
 % add program report and dependency checker
 st = dbstack('-completenames'); % List of calling programs
 st = flipud(st);
-eh4 = uimenu(mh, LabelName, 'Calling Programs');
-eh4_files = cell(1, numel(st) + 1);
+eh5 = uimenu(mh, LabelName, 'Calling Programs');
+eh5_files = cell(1, numel(st) + 1);
 for k = 1:numel(st)  
     [~,f_name] = fileparts(st(k).file); % only the file name , not path
     if strcmp(f_name , 'cmenu') == 0 % cmenu will not be listed.
-        eh4_files{k} = uimenu(eh4, LabelName,  f_name );
-        eh4_files{k}.MenuSelectedFcn=@(~,~) dependfigure_cmenu(st(k).file,eh4_files{k});
+        eh5_files{k} = uimenu(eh5, LabelName,  f_name );
+        eh5_files{k}.MenuSelectedFcn=@(~,~) dependfigure_cmenu(st(k).file,eh5_files{k});
         
     end
 end
@@ -178,6 +181,10 @@ end
 
 end
 
+%% Figure Size Fix.
+
+
+
 
 %% Dependency list
 function dependfigure_cmenu(filenamecell, eh4_files)
@@ -199,9 +206,53 @@ pList = reshape(pList,[],1);
 
 productList = {pList.Name};
 productList = reshape(productList,[],1);
- 
-% copy to clipboard
-clipboard('copy', strjoin([fList; productList], newline));
 
-disp('Dependency Lists were copied to your clipboard');
+% #copy to clipboard (Version 1, Full file lists.)
+% clipboard('copy', strjoin([fList; productList], newline));
+% disp('Dependency Lists were copied to your clipboard');
+
+% #TidyUp
+dnames = cell(numel(fList),3); % dependency file names.
+% separate to directory
+for k = 1:numel(fList)
+   [dnames{k,1} , fn,ext] = fileparts(fList{k});
+   dnames{k,2} = [sprintf('\t'), fn,ext];
+end
+[~, ia] = unique(dnames(:,1),'stable'); % Remove unique folders
+for k = 1:numel(ia)
+    dnames{ia(k),3} = dnames{ia(k),1};
+end
+%Re-sorting
+tx = reshape(dnames(:,[3 2]).',[],1);
+tx(cellfun(@isempty,tx)) = [];
+
+% Copy to clipboard (version 2, partial file lists)
+clipboard('copy', strjoin([tx; productList], newline));
+
+% Save to the file.
+%fp = fopen(['dependenciesfilenamecell{1});
+[filedir, file, ~] = fileparts(filenamecell{1});
+ext = '_dependence.txt'; k = 1; sw = 0;
+while sw == 0
+    fn2 = fullfile(filedir, [file ext]);
+    if exist(fn2, 'file') % when the file exists.
+        % File exists.
+        ext = ['_dependence' num2str(k) '.txt'];
+        k = k + 1;
+    else
+        % You can use this file.
+        sw = 1;
+    end
+end
+fp = fopen(fn2,'w');
+if fp == -1
+    error('Error in creating file');
+else
+    % Write the string to the file.
+   fprintf(fp, '%s', strjoin([tx; productList], newline));
+   fclose(fp);
+   disp('Dependency Lists were succesfully saved at :');
+   disp(fn2);
+end
+
 end
